92b059b79eaae82869e963072848c923ad8ee911
aniket8200 <cs5160398@cse.iitd.ac.in>
2019-12-27 01:25:59 +0530

    Args added initiateLoan.js

--- application/initiateLoan.js
+++ application/initiateLoan.js
@@ -5,12 +5,96 @@ const fs = require('fs');
 const yaml = require('js-yaml');
 const { FileSystemWallet, Gateway } = require('fabric-network');
 // const Lender = require('../')
+var ArgumentParser = require('argparse').ArgumentParser;
+
+
+
+function getArguments() {
+	var parser = new ArgumentParser({
+		version: '0.0.1',
+		addHelp : true
+	});
+
+	parser.addArgument(
+		['-li', '--lenderid'],
+		{
+			help: 'Lender\'s Identity (x.509 certifcate)',
+			metavar:'',
+			required:true
+		}
+	);
+	parser.addArgument(
+		['-bi', '--borrowerid'],
+		{
+			help: 'Borrower\'s Identity (x.509 certifcate)',
+			metavar:'',
+			required:true
+		}
+	);
+
+	parser.addArgument(
+		['-la', '--amount'],
+		{
+			help: 'Loan amount.',
+			metavar:'',
+			required:true
+		}
+	);
+	
+	parser.addArgument(
+		['-as', '--assets'],
+		{
+			help: 'Assets taken',
+			metavar:'',
+			required:true
+		}
+	);
+	parser.addArgument(
+		['-int', '--interest'],
+		{
+			help: 'Interest taken',
+			metavar:'',
+			required:true
+		}
+	);
+	parser.addArgument(
+		['-ld', '--lastDate'],
+		{
+			help: 'Last Installment Date',
+			metavar:'',
+			required:true
+		}
+	);
+	parser.addArgument(
+		['-nd', '--nextDate'],
+		{
+			help: 'Next Installment Date',
+			metavar:'',
+			required:true
+		}
+	);
+	parser.addArgument(
+		['-na', '--nextAmount'],
+		{
+			help: 'Next Installment Amount',
+			metavar:'',
+			required:true
+		}
+	);
+
+
+	var args = parser.parseArgs();
+	return args;
+}
+
 
 async function main() {
 
 // 	Arguments : Username, wallet, channelName, lenderId, borrowerId, loanAmount, assets, interest, lastInstallmentDate, nextInstallmentDate, nextInstallmentAmount
-
 	const gateway = new Gateway();
+	var args = getArguments();
+	console.dir(args);
+
 
 	try {
 
@@ -28,10 +112,10 @@ async function main() {
 
 	    const contract = await network.getContract('contract');
 
-	    const initiateLoanResponse = await contract.submitTransaction("initiateLoan", lenderId, borrowerId, loanAmount, assets, interest, lastInstallmentDate, nextInstallmentDate, nextInstallmentAmount)
+	    const initiateLoanResponse = await contract.submitTransaction("initiateLoan", args.lenderId, args.borrowerId, args.mount, args.assets, args.interest, args.lastDate, args.nextDate, args.nextAmount);
 
 	    console.log("Loan application initiated.");
-	    console.log(`Loan Key: ${initiateLoanResponse}`)
+	    console.log(`Loan Key: ${initiateLoanResponse}`);
 	    console.log("Transaction Completed");
 	}
 
